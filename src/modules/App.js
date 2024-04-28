23df60996c895d30c0ad42895936156b531c8682
Trino <trinogamer11@gmail.com>
2024-04-28 17:49:29 +0200

    First commit

---
+++ src/modules/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+
+const isNative = () => {
+  // const urlParams = new URLSearchParams(window.location.search);
+  // return urlParams.get('app');
+  return navigator.userAgent.includes('Electron');
+}
+
+window.tvodsconfig = {
+  MANIFEST_URL: 'https://cdn.trinohost.com/webdavshare/tmods/manifest.json',
+  VERSION: '0.2.2',
+  NATIVE_APP: isNative(),
+};
+
+document.title += (" | " + (window.tvodsconfig.NATIVE_APP ? 'app nativa' : 'navegador'));
+
+function App() {
+  return (
+    <Router>
+      <div className='App'>
+        <main>
+        </main>
+      </div>
+    </Router>
+  );
+}
+
+export default App;

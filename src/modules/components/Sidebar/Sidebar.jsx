ad152c56be7b74944cbb60b2b741195f81ed2ab3
trino <rebollar.marquez.antoniomanuel@gmail.com>
2024-05-01 20:38:41 +0200

    Sidebar styling

--- src/modules/components/Sidebar/Sidebar.jsx
+++ src/modules/components/Sidebar/Sidebar.jsx
@@ -7,29 +7,29 @@ import WhatshotIcon from '@mui/icons-material/Whatshot';
 import ScheduleIcon from '@mui/icons-material/Schedule';
 
 function Sidebar() {
-  return <>
+  return <div className='h-screen bg-slate-100 border-r-2'>
     <ul>
       <li>
-        <NavLink to="/home" className="" activeClassName="">
-          <HomeIcon className="icon" />
+        <NavLink to="/home" className="sidebar-pill sidebar-pill-active" activeClassName="sidebar-pill-active">
+          <HomeIcon className="sidebar-icon" />
           Home
         </NavLink>
       </li>
       <li>
-        <NavLink to="/recents" className="" activeClassName="">
-          <ScheduleIcon className="icon" />
+        <NavLink to="/recents" className="sidebar-pill" activeClassName="sidebar-pill-active">
+          <ScheduleIcon className="sidebar-icon" />
           Recents
         </NavLink>
       </li>
       <li>
-        <NavLink to="/top" className="" activeClassName="">
-          <WhatshotIcon className="icon" />
+        <NavLink to="/top" className="sidebar-pill" activeClassName="sidebar-pill-active">
+          <WhatshotIcon className="sidebar-icon" />
           Top
         </NavLink>
       </li>
     </ul>
     <Outlet />
-  </>
+  </ div>
 }
 
 export default Sidebar;

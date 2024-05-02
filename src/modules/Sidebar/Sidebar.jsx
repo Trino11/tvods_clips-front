5c94f3f7f873216633f7666970c4280adcf03b67
trino <rebollar.marquez.antoniomanuel@gmail.com>
2024-05-02 23:05:07 +0200

    Sidebar styling and lint

--- src/modules/Sidebar/Sidebar.jsx
+++ src/modules/Sidebar/Sidebar.jsx
@@ -2,13 +2,21 @@ import './Sidebar.css';
 
 import { Outlet, NavLink } from 'react-router-dom';
 
+import TvIcon from '@mui/icons-material/Tv';
 import HomeIcon from '@mui/icons-material/Home';
 import WhatshotIcon from '@mui/icons-material/Whatshot';
 import ScheduleIcon from '@mui/icons-material/Schedule';
 
 export default function Sidebar() {
-  return <div className='h-screen bg-slate-100 border-r-2'>
-    <ul className='py-4'>
+  return <div className='bg-slate-100 py-4 border-r-2 h-screen'>
+    <NavLink to='/' className='pb-0 font-semibold text-2xl text-black sidebar-pill'>
+      <TvIcon className='sidebar-icon' />
+      TVods
+    </NavLink>
+
+    <div className="mx-3 my-3 border-t-2" />
+
+    <ul className=''>
       <li>
         <NavLink to='/home' className={({isActive}) => 'sidebar-pill ' + (isActive ? 'sidebar-pill-active': '')}>
           <HomeIcon className='sidebar-icon' />

405b18b825752c5d85631d370615b9385df27821
Trino <trinogamer11@gmail.com>
2024-05-28 21:56:03 +0200

    General cards styling

--- src/modules/Videos/components/VideoCard.tsx
+++ src/modules/Videos/components/VideoCard.tsx
@@ -1,20 +1,20 @@
 import React from 'react'
 import { VideoModel } from '../models/VideoModel'
 
-export default function VideoCard({ video }: { video: VideoModel }) {
-  return <div className='max-w-96'>
-    <div className='relative w-96 h-52 overflow-hidden'>
-      <img src={video.splash} className='rounded-lg cover-fill' />
-      <p className='right-2 bottom-2 absolute bg-gray-700 bg-opacity-50 px-2 h-min text-white'>{video.duration}</p>
+export default function VideoCard({ video, size = 'fixed' }: { video: VideoModel | undefined, size?: 'fixed' | 'auto' }) {
+  return <div className={' ' + (size === 'fixed' ? 'mx-1 w-96' : 'px-1 mb-3 md:w-1/2 lg:w-1/3 2xl:w-1/4 w-full') + ' ' + (video === undefined ? 'opacity-0' : '')}>
+    <div className={'relative overflow-hidden ' + (size === 'fixed' ? 'w-96' : '')}>
+      <img src={video?.splash} className='rounded-lg aspect-video cover-fill' alt={video?.title} />
+      <p className='right-2 bottom-2 absolute bg-gray-700 bg-opacity-50 px-2 rounded-md h-min text-white'>{video?.duration}</p>
     </div>
     <div className='flex mt-2'>
-      <div className='w-10'>
-        <img src={video.author.picture} className='rounded-full' />
+      <div className='w-10 min-w-10'>
+        <img src={video?.author.picture} className='rounded-full' alt={video?.author.alias} />
       </div>
       <div className='ml-4 leading-none'>
-        <h3 className='mb-1 font-semibold text-xl'>{video.title}</h3>
-        <h4>{video.author.alias}</h4>
-        <p>{video.views} views · {video.likes} likes · {video.uploadDate.toDateString()}</p>
+        <h3 className='mb-1 font-semibold text-xl'>{video?.title}</h3>
+        <h4>{video?.author.alias}</h4>
+        <p>{video?.views} views · {video?.likes} likes · {video?.uploadDate.toDateString()}</p>
       </div>
     </div>
   </div>
--- src/modules/core/design-system/CategoryHolder.tsx
+++ src/modules/core/design-system/CategoryHolder.tsx
@@ -4,20 +4,26 @@ import { NavLink } from 'react-router-dom';
 
 import CallMadeIcon from '@mui/icons-material/CallMade';
 
-import { VideoModel } from '../../Videos/models/VideoModel.ts';
 import VideoCard from '../../Videos/components/VideoCard.tsx';
 
-export default function CategoryHolder({ title = 'placeholder', route, type, videos, children } : { title: string, route?: string, type?: 'horizontal' | 'vertical', videos: Function, children?: ReactNode}) {
+export default function CategoryHolder({ title = 'placeholder', route, type = 'horizontal', videos, children } : { title: string, route?: string, type?: 'horizontal' | 'vertical', videos: Function, children?: ReactNode}) {
+  const videosToShow: ReactNode[] = videos().map((video) => {
+    return <VideoCard video={video} size={type === 'vertical' ? 'auto' : 'fixed'} />
+  });
+
+  if (type === 'vertical')
+    while(videosToShow.length % 3 !== 0) {
+      videosToShow.push(<VideoCard video={undefined} size={type === 'vertical' ? 'auto' : 'fixed'} />);
+    }
+
   return <div className="block my-2 pl-4">
     <NavLink to={'/' + (route ? route : title.toLowerCase())} className='flex items-end mb-2 pr-4 w-fit'>
       <h2 className="mr-2 font-semibold text-2xl">{title}</h2>
       <CallMadeIcon />
     </NavLink>
 
-    <div className={'flex ' + (type === 'vertical' ? 'flex-wrap' : 'flex-row') + ' gap-4 pb-2 overflow-x-scroll overflow-y-hidden scroll-custom'}>
-      {videos().map((video) => {
-        return <VideoCard video={video} />
-      })}
+    <div className={'flex ' + (type === 'vertical' ? 'flex-wrap justify-center' : 'flex-row') + ' pb-2 overflow-x-scroll overflow-y-hidden scroll-custom pr-4'}>
+      {videosToShow}
     </div>
   </div>
 }
